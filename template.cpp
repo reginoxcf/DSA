7905b8c2b8939d023e5c33217beb40764698a5ec
reginox <99781653+reginoxcf@users.noreply.github.com>
2025-05-28 10:43:10 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,8 +4,8 @@
 #define all(v) begin(v), end(v)
 #define pi pair<int, int>
 #define vi vector<int>
-
 using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
