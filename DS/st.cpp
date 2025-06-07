84a524a5a1c46f3216871a44e2f7613e46e33cca
reginoxcf <1605giabao@gmail.com>
2025-06-07 22:21:34 +0700

    update segtree

--- DS/st.cpp
+++ DS/st.cpp
@@ -1,26 +1,37 @@
-const ll N = 1e6+3;
+struct node{
+    //define node here
+    long long x;
+    explicit node(long long _x = 0){
+
+    }
+
+    node operator+(const node &o) const{
+        return node();
+    };
+};
+
 struct SegTree{
-    //Segment tree subarray sum
-    ll tree[N*4+1];
-    void reset(){
-        memset(tree,0,sizeof(tree));
+    vector<node> tree;
+    void resize(int n){
+        tree.assign(n * 4 + 3, node());
     }
-    void upd(ll id, ll l, ll r, ll u, ll val){
-        if(r<u||u<l) return ;
-        if(l==r){
-            tree[id]=val;
-            return;
+    void update(int id, int l, int r, int u, node v){
+        if(r < u || u < l) return ;
+        if(l == r){
+            tree[id] = v;
+            return ;
         }
-        ll mid=(l+r)>>1;
-        upd(id*2,l,mid,u,val);
-        upd(id*2+1,mid+1,r,u,val);
-        tree[id]=tree[id*2]+tree[id*2+1];
+        int mid = l + (r-l)/2;
+        update(id<<1, l, mid, u, v);
+        update(id<<1|1, mid+1, r, u, v);
+        tree[id] = tree[id<<1] + tree[id<<1|1];
     }
-    ll get(ll id, ll l, ll r, ll u, ll v){
-        if(u>v||l>r) return 0;
-        if(v<l||r<u) return 0;
-        if(u<=l&&r<=v) return tree[id];
-        ll mid=(l+r)>>1;
-        return get(id*2, l, mid, u, v)+get(id*2+1, mid+1, r, u, v);
+    node get(int id, int l, int r, int u, int v){
+        if(r < u || v < l) return node(0);
+        if(u <= l && r <= v) return tree[id];
+        int mid = l + (r-l)/2;
+        node lf = get(id<<1, l, mid, u, v);
+        node rg = get(id<<1|1, mid+1, r, u, v);
+        return lf + rg;
     }
-} tree;
+};
