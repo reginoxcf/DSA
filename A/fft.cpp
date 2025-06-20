87cc32a67a055843e2c6c492c6264513d942ea18
reginoxcf <1605giabao@gmail.com>
2025-06-20 16:23:50 +0700

    fft

---
+++ A/fft.cpp
@@ -0,0 +1,44 @@
+void fft(vector<cd> &a, bool invert) {
+    int n = a.size(), L = __builtin_ctz(n);
+    vector<int> rev(n);
+    for (int i = 0; i < n; i++) {
+        rev[i] = (rev[i >> 1] | (i & 1) << L) >> 1;
+        if (i < rev[i]) swap(a[i], a[rev[i]]);
+    }
+    for (int len = 2; len <= n; len <<= 1) {
+        cd wlen = polar(1.0L, acos(-1.0L) / len * (invert ? -2 : 2));
+        for (int i = 0; i < n; i += len) {
+            cd w = 1;
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i + j];
+                cd v = a[i + j + len / 2] * w;
+                a[i + j] = u + v;
+                a[i + j + len / 2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+    if (invert) {
+        for (auto &x : a) x /= n;
+    }
+}
+
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
