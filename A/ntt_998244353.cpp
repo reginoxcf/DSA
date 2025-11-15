b7f3bd4784d9ac23fcad7b7959bc9cc89e694405
reginox <99781653+reginoxcf@users.noreply.github.com>
2025-11-15 20:58:12 +0700

    indent

--- A/ntt_998244353.cpp
+++ A/ntt_998244353.cpp
@@ -1,44 +1,44 @@
 const int mod = 998244353, root = 15311432, root_1 = 469870224, root_pw = 1LL<<23;
  
 int binpow(int a, int b){
-    int res = 1;
-    while(b > 0){
-        if(b & 1){
-            res = 1LL * res * a % mod;
-        }
-        a = 1LL * a * a % mod;
-        b >>= 1;
+  int res = 1;
+  while(b > 0){
+    if(b & 1){
+      res = 1LL * res * a % mod;
     }
-    return res;
+    a = 1LL * a * a % mod;
+    b >>= 1;
+  }
+  return res;
 }
  
 void ntt(vector<int>& a, bool invert){
-    int n = a.size();
-    for (int i = 1, j = 0; i < n; i++){
-        int bit = (n >> 1);
-        for (; j & bit; bit >>= 1){
-            j ^= bit;
-        } 
-        j ^= bit;
-        if(i < j) swap(a[i], a[j]);
+  int n = a.size();
+  for (int i = 1, j = 0; i < n; i++){
+    int bit = (n >> 1);
+    for (; j & bit; bit >>= 1){
+      j ^= bit;
+    } 
+    j ^= bit;
+    if(i < j) swap(a[i], a[j]);
+  }
+  for (int l = 2; l <= n; l <<= 1){
+    int wl = invert ? root_1 : root;
+    for(int i = l; i < root_pw; i <<= 1) wl = 1LL * wl * wl % mod;
+    for(int i = 0; i < n; i += l){
+      int w = 1;
+      for (int j = 0; j < l / 2; j++){
+        int u = a[i + j], v = 1LL * a[i + j + l / 2] * w % mod;
+        a[i + j] = ((u + v) < mod) ? (u+v) : (u+v-mod);
+        a[i + j + l / 2] = ((u - v) >= 0) ? (u - v) : (u - v + mod);
+        w = 1LL * w * wl % mod;
+      }
     }
-    for (int l = 2; l <= n; l <<= 1){
-        int wl = invert ? root_1 : root;
-        for(int i = l; i < root_pw; i <<= 1) wl = 1LL * wl * wl % mod;
-        for(int i = 0; i < n; i += l){
-            int w = 1;
-            for (int j = 0; j < l / 2; j++){
-                int u = a[i + j], v = 1LL * a[i + j + l / 2] * w % mod;
-                a[i + j] = ((u + v) < mod) ? (u+v) : (u+v-mod);
-                a[i + j + l / 2] = ((u - v) >= 0) ? (u - v) : (u - v + mod);
-                w = 1LL * w * wl % mod;
-            }
-        }
-    }
-    if(invert){
-        int n_1 = binpow(n, mod - 2);
-        for(int& i : a) i = (1LL * i * n_1) % mod;
-    }
+  }
+  if(invert){
+    int n_1 = binpow(n, mod - 2);
+    for(int& i : a) i = (1LL * i * n_1) % mod;
+  }
 }
  
 vector<int> multiply(vector<int> const& a, vector<int> const& b){
