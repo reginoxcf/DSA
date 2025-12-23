df77ddd6a814f05e4bccc5f32081a62f4eb352b4
reginox <99781653+reginoxcf@users.noreply.github.com>
2025-12-23 09:29:23 +0700

    Refactor matrix multiplication and power functions

--- A/matexp.cpp
+++ A/matexp.cpp
@@ -1,30 +1,25 @@
-const ll MOD = 1e9+7;
+const ll mod = 1e9+7;
 
-vector<vector<ll>> mmul(const vector<vector<ll>> &a, const vector<vector<ll>> &b){
-    vector<vector<ll>> c;
-    c.resize(a.size(), vector<ll>(b[0].size()));
-    for(int i = 0; i < a.size(); i++){
-        for(int j = 0; j < b[0].size(); j++){
-            for(int k = 0; k < a[0].size(); k++){
-                c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % MOD;
-            }
-        }
+vector<vector<int>> mmul(const vector<vector<int>> &a, const vector<vector<int>> &b){
+  vector<vector<int>> c;
+  c.resize(a.size(), vector<int>(b[0].size()));
+  for(int i = 0; i < a.size(); i++){
+    for(int j = 0; j < b[0].size(); j++){
+      for(int k = 0; k < a[0].size(); k++){
+        c[i][j] = (1ll * a[i][k] * b[k][j] + 1ll * c[i][j]) % mod;
+      }
     }
-    return c;
+  }
+  return c;
 }
 
-vector<vector<ll>> mpow(vector<vector<ll>> b, ll m){
-    vector<vector<ll>> res = b; 
-    for(int i = 0; i < res.size(); i++){
-        for(int j = 0; j < res[i].size(); j++){
-            if(i == j) res[i][j] = 1;
-            else res[i][j] = 0;
-        }
-    }
-    while(m){
-        if(m & 1) res = mmul(res, b);
-        b = mmul(b, b);
-        m >>= 1;
-    }
-    return res;
+vector<vector<int>> mpow(vector<vector<int>> b, ll m){
+  vector<vector<int>> res(b.size(), vector<int>(b[0].size(), 0));
+  for(int i = 0; i < res.size(); i++) res[i][i] = 1;
+  while(m){
+    if(m & 1) res = mmul(res, b);
+    b = mmul(b, b);
+    m >>= 1;
+  }
+  return res;
 }
