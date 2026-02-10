7c4060e67045883e6f67fefc9fede623bf845b84
reginox <99781653+reginoxcf@users.noreply.github.com>
2026-02-10 08:24:08 +0700

    hld

--- A/hld.cpp
+++ A/hld.cpp
@@ -1,7 +1,6 @@
 struct HLD{
   vector<vector<int>> g;
   vector<int> sz, depth, anc;
-  vector<pair<int, int>> hid;
   vector<vector<int>> sp;
   vector<int> tin, tout;
   int hsz = 0, timer = 0, n = 0, L = 0;
@@ -12,7 +11,6 @@ struct HLD{
     g.assign(n + 1, vector<int>());
     sz.assign(n + 1, 0);
     depth = anc = tin = tout = sz;
-    hid.assign(n + 1,{-1, -1});
     sp.assign(n + 1, vector<int>(L + 1, 0));
     hsz = 0;
     timer = 0;
@@ -31,45 +29,38 @@ struct HLD{
       if(v == p) continue;
       sp[v][0] = u;
       dfs_sz(v, u);
-      sz[u] += sz[v];
+      sz[u] += sz[v]; 
       if(sz[v] > sz[g[u][0]]) swap(g[u][0], v);
     }
   }
 
   void dfs_hld(int u = 1, int p = 0){
-    if(hid[u].first == -1){
-      hid[u].first = ++hsz;
-      hid[u].second = 1;
-      anc[u] = u;
-    }
+    if(p == 0) anc[u] = u;
     tin[u] = ++timer;
     for(auto v : g[u]){
       if(v == p) continue;
-      if(v == g[u][0]){
-        hid[v].first = hid[u].first;
-        hid[v].second = hid[u].second + 1;
-        anc[v] = anc[u];
-      }
+      if(v == g[u][0]) anc[v] = anc[u];
+      else anc[v] = v;
       dfs_hld(v, u);
     }
     tout[u] = timer;
   }
   
-    int LCA(int u, int v){
-      if(depth[u] > depth[v]) swap(u, v);
-      int diff = depth[v] - depth[u];
-      for(int l = L; l >= 0; l--){
-        if((diff >> l) & 1) v = sp[v][l];
-      }
-      if(u == v) return u;
-      for(int l = L; l >= 0; l--){
-        if(sp[u][l] != sp[v][l]){
-          u = sp[u][l];
-          v = sp[v][l];
-        }
-      }
-      return sp[u][0];
+  int LCA(int u, int v){
+    if(depth[u] > depth[v]) swap(u, v);
+    int diff = depth[v] - depth[u];
+    for(int l = L; l >= 0; l--){
+      if((diff >> l) & 1) v = sp[v][l];
     }
+    if(u == v) return u;
+    for(int l = L; l >= 0; l--){
+      if(sp[u][l] != sp[v][l]){
+        u = sp[u][l];
+        v = sp[v][l];
+      }
+    }
+    return sp[u][0];
+  }
 
   void go(int u = 1){
     dfs_sz(u);
