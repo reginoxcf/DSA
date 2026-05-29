825aa133386b502902c74bc4e874bb083aecc18d
reginox <99781653+reginoxcf@users.noreply.github.com>
2026-05-29 14:54:49 +0700

    Update geo.cpp

--- A/geo.cpp
+++ A/geo.cpp
@@ -26,6 +26,8 @@ struct point {
 	point perp() const {return point(Oy, -Ox);} // rotate 90 ccw
 	ld length() const {return sqrtl(Ox * Ox + Oy * Oy);}
 	ld dist(const point& A) const {return (A - *this).length();}
+
+  bool operator==(const point &o){return Ox==o.Ox && Oy==o.Oy;}
 };
  
 bool acute(const point& A, const point& B, const point& C) { // checks angle ABC
@@ -34,6 +36,13 @@ bool acute(const point& A, const point& B, const point& C) { // checks angle ABC
  
 ld angle(const point& A, const point& B, const point& C) {
 	return acosl(min(1.0L, (A - B).dot(C - B) / ((A - B).length() * (C - B).length())));
+}
+
+int orientation(const point& A, const point& B, const point& C) {
+  // Returns the orientation sign: +1 for CCW, -1 for CW, 0 for collinear/flat
+  ld cross_product = A.cross(B, C);
+  if (abs(cross_product) < eps) return 0;
+  return (cross_product > 0) ? 1 : -1;
 }
  
 struct line {
@@ -63,4 +72,37 @@ struct line {
 ld triarea(ld a, ld b, ld c){
   ld p = (a + b + c)/2;
   return sqrtl(p*(p-a)*(p-b)*(p-c));
+}
+
+point circumcenter(point a, point b, point c){
+  ld d = 2 * (a.Ox * (b.Oy - c.Oy) + b.Ox * (c.Oy - a.Oy) + c.Ox * (a.Oy - b.Oy));
+  if(d == 0) return point(inf, inf);
+  else{
+    ld xO = (a.length() * a.length() * (b.Oy - c.Oy) + b.length() * b.length() * (c.Oy - a.Oy) + c.length() * c.length() * (a.Oy - b.Oy)) / d;
+    ld yO = (a.length() * a.length() * (c.Ox - b.Ox) + b.length() * b.length() * (a.Ox - c.Ox) + c.length() * c.length() * (b.Ox - a.Ox)) / d;
+    return point(xO, yO);
+  }
+}
+
+point reflect_point(point p, point c){
+  return point(2*c.Ox - p.Ox, 2*c.Oy - p.Oy);
+}
+
+pair<line, line> angle_bisector(const line& l1, const line& l2){
+  ld norm1 = sqrtl(l1.a * l1.a + l1.b * l1.b);
+  ld norm2 = sqrtl(l2.a * l2.a + l2.b * l2.b);
+
+  line b1, b2;
+
+  // First bisector (+)
+  b1.a = l1.a * norm2 - l2.a * norm1;
+  b1.b = l1.b * norm2 - l2.b * norm1;
+  b1.c = l1.c * norm2 - l2.c * norm1;
+
+  // Second bisector (-)
+  b2.a = l1.a * norm2 + l2.a * norm1;
+  b2.b = l1.b * norm2 + l2.b * norm1;
+  b2.c = l1.c * norm2 + l2.c * norm1;
+
+  return {b1, b2};
 }
